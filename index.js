a0fc94fd69530815cd23b4fbc712119e8bc1afcb
g23fsdv <gs1amba2088@hotmail.com>
2024-05-06 12:15:02 +0700

    update commit

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function sortColors(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  let i = 0;
-  while (i <= right) {
-    if (nums[i] === 0) {
-      [nums[i], nums[left]] = [nums[left], nums[i]];
-      left++;
-      i++;
-    } else if (nums[i] === 2) {
-      [nums[i], nums[right]] = [nums[right], nums[i]];
-      right--;
-    } else {
-      i++;
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
+  return false;
 }
