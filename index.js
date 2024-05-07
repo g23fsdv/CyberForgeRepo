58fd9111b608eb6de3630ce5d8583ce21016e56f
g23fsdv <gs1amba2088@hotmail.com>
2024-05-07 13:21:01 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
+function findSubstring(s, words) {
+  if (s.length === 0 || words.length === 0) return [];
+  const wordMap = new Map();
+  for (const word of words) {
+    wordMap.set(word, (wordMap.get(word) || 0) + 1);
   }
-  return false;
+  const wordLength = words[0].length;
+  const totalLength = words.length * wordLength;
+  const result = [];
+  for (let i = 0; i <= s.length - totalLength; i++) {
+    const substring = s.substring(i, i + totalLength);
+    if (isValid(substring, new Map(wordMap))) {
+      result.push(i);
+    }
+  }
+  return result;
+  function isValid(substring, wordMap) {
+    const wordCount = new Map();
+    for (let i = 0; i < substring.length; i += wordLength) {
+      const word = substring.substring(i, i + wordLength);
+      wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    }
+    for (const [key, value] of wordMap) {
+      if ((wordCount.get(key) || 0) !== value) return false;
+    }
+    return true;
+  }
 }
