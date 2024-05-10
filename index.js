0412acfbe11ca370a6898a16df65792c20d35a87
g23fsdv <gs1amba2088@hotmail.com>
2024-05-10 08:10:01 +0700

    init project

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
+    }
   }
-  return prev;
+  return stack.join("");
 }
